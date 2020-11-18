05b0610788949bda681c3e0a01b428d97a31144d
Ludwig Mueller <schnitzelerwin@gmail.com>
2020-11-17 20:33:58 -0500

    running background works

--- messing/GameScene.swift
+++ messing/GameScene.swift
@@ -9,6 +9,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var bground = SKSpriteNode()
+    
+    
     private var redBall : SKSpriteNode?
     
     override func didMove(to view: SKView) {
@@ -20,6 +23,8 @@ class GameScene: SKScene {
         redBall?.position = CGPoint(x: 0, y: 400)
         
         self.addChild(redBall!)
+        
+        createBGround()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -43,5 +48,32 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        moveBGround()
+    }
+    
+    func createBGround(){
+        for i in 0...3 {
+         
+            let bground = SKSpriteNode(imageNamed: "background")
+            bground.name =  "BGround"
+            bground.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!)
+            bground.anchorPoint = CGPoint(x: 0, y: 0.5)
+            bground.position = CGPoint(x: -bground.frame.width/2, y: CGFloat(i) * bground.size.width)
+            self.addChild(bground)
+        }
+    }
+    
+    func moveBGround(){
+        
+        self.enumerateChildNodes(withName: "BGround", using: ({
+            (node, error) in
+            
+            node.position.y -= 2
+            
+            if node.position.y < -((self.scene?.size.height)!) {
+                
+                node.position.y += (self.scene?.size.height)! * 3
+            }
+        }))
     }
 }
